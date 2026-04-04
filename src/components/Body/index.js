deee951acc04172fb4e98d75c5faf3f6740d482d
Naveen <knvalwayslike7@gmail.com>
2026-04-04 19:57:28 +0000

    redesigned code

--- src/components/Body/index.js
+++ src/components/Body/index.js
@@ -1,10 +1,20 @@
 import {Component} from 'react'
+import Loader from 'react-loader-spinner'
 import CategoryMenuList from '../CategoryMenuList'
 import MenuItemsContainer from '../MenuItemsContainer'
 import ResContext from '../../context/ResContext'
+import './index.css'
+
+const apiConstants = {
+  initial: 'INITIAL',
+  inProgress: 'INPROGRESS',
+  success: 'SUCCESS',
+  failure: 'FAILURE',
+}
 
 class Body extends Component {
   state = {
+    apiStatus: apiConstants.initial,
     menuList: [],
     activeMenuId: '',
   }
@@ -14,45 +24,58 @@ class Body extends Component {
   }
 
   getMenuData = async () => {
-    const response = await fetch(
-      'https://apis2.ccbp.in/restaurant-app/restaurant-menu-list-details',
-    )
-    const data = await response.json()
-    const restaurantData = data[0]
+    this.setState({apiStatus: apiConstants.inProgress})
 
-    const formattedMenuList = restaurantData.table_menu_list.map(menu => ({
-      menu_category: menu.menu_category,
-      menu_category_id: menu.menu_category_id,
-      category_dishes: menu.category_dishes.map(dish => ({
-        dish_id: dish.dish_id,
-        dish_name: dish.dish_name,
-        dish_price: dish.dish_price,
-        dish_currency: dish.dish_currency,
-        dish_description: dish.dish_description,
-        dish_image: dish.dish_image,
-        dish_calories: dish.dish_calories,
-        dish_Availability: dish.dish_Availability,
-        dish_Type: dish.dish_Type,
-        addonCat: dish.addonCat || [],
-      })),
-    }))
+    try {
+      const response = await fetch(
+        'https://apis2.ccbp.in/restaurant-app/restaurant-menu-list-details',
+      )
 
-    this.setState({
-      menuList: formattedMenuList,
-      activeMenuId: formattedMenuList[0].menu_category_id,
-    })
+      if (!response.ok) {
+        throw new Error('Failed to fetch')
+      }
 
-    const {setRestaurantName} = this.context
-    setRestaurantName(restaurantData.restaurant_name)
+      const data = await response.json()
+      const restaurantData = data[0]
+
+      const formattedMenuList = restaurantData.table_menu_list.map(menu => ({
+        menu_category: menu.menu_category,
+        menu_category_id: menu.menu_category_id,
+        category_dishes: menu.category_dishes.map(dish => ({
+          dish_id: dish.dish_id,
+          dish_name: dish.dish_name,
+          dish_price: dish.dish_price,
+          dish_currency: dish.dish_currency,
+          dish_description: dish.dish_description,
+          dish_image: dish.dish_image,
+          dish_calories: dish.dish_calories,
+          dish_Availability: dish.dish_Availability,
+          dish_Type: dish.dish_Type,
+          addonCat: dish.addonCat || [],
+        })),
+      }))
+
+      this.setState({
+        menuList: formattedMenuList,
+        activeMenuId: formattedMenuList[0].menu_category_id,
+        apiStatus: apiConstants.success,
+      })
+
+      const {setRestaurantName} = this.context
+      setRestaurantName(restaurantData.restaurant_name)
+    } catch (error) {
+      this.setState({apiStatus: apiConstants.failure})
+      const {setRestaurantName} = this.context
+      setRestaurantName('UNI Resto Cafe')
+    }
   }
 
   changeActiveMenu = id => {
     this.setState({activeMenuId: id})
   }
 
-  render() {
+  renderSuccessView = () => {
     const {menuList, activeMenuId} = this.state
-
     return (
       <>
         <CategoryMenuList
@@ -63,6 +86,38 @@ class Body extends Component {
         <MenuItemsContainer menuList={menuList} activeMenuId={activeMenuId} />
       </>
     )
+  }
+
+  renderFailureView = () => (
+    <div className="retry-container">
+      <p>Failed to fetch details</p>
+      <button type="button" onClick={this.getMenuData} className="retry-button">
+        Retry
+      </button>
+    </div>
+  )
+
+  renderFinalView = () => {
+    const {apiStatus} = this.state
+
+    switch (apiStatus) {
+      case apiConstants.inProgress:
+        return (
+          <div className="loader-container">
+            <Loader type="Oval" color="#680707" height={80} width={80} />
+          </div>
+        )
+      case apiConstants.success:
+        return this.renderSuccessView()
+      case apiConstants.failure:
+        return this.renderFailureView()
+      default:
+        return null
+    }
+  }
+
+  render() {
+    return this.renderFinalView()
   }
 }
 
